c1b8c1b52d48c8d306adf72f298e7d6449293de8
badoleprajwal853-hub <badoleprajwal853@gmail.com>
2026-04-07 12:47:43 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,16 @@
 variable"environment"{
 description ="deployment environment"
-type        =
+type        =string
+default     ="dev"
+validation{
+condition=contains(
+["dev","staging","prod"],
+var.environment)
+error_massage="Invailid env"
+}
+}
+variable "instance_type"{
+description="EC2instance type"
+type       = string
+default    ="t3.micro"
+}
