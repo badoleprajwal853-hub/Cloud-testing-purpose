dbabc0598db2300e0cbb0a528bd2eabc6d2b7db7
badoleprajwal853-hub <badoleprajwal853@gmail.com>
2026-04-07 12:37:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,22 @@
 # main file 
+#Resource block syntax:
+#resource "<PROVIDER>_<TYPE>""<NAME>"
+
+resource "aws_instance""web_server"{
+ami           =var.ami_id
+instance_type+var.instance_type
+
+tag={
+Name       ="web-${var.enviroment
+Enviroment =var.envirment
+ManagedBy  ="terraform"
+}
+
+root_block_device{
+volume_size=20
+encrypted=true
+}
+}
+#Reference another resource:
+resource"aws_eip""web_ip"{
+instance=aws_instance.web_server.id
